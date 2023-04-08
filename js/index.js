1a156b240b4fafeeace612e88b9d870d3dd09350
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2023-04-08 17:28:02 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const changableText = document.getElementById("api_changes")
+const subtitle = document.getElementById("subtitle")
+
+document.getElementById("btn").addEventListener("click", function() {
+  changableText.classList.remove("api_changes")  
+  subtitle.remove(subtitle)
+
+  fetch("http://www.boredapi.com/api/activity/")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    changableText.classList.add("api_changes")      
+    changableText.textContent = data.activity 
+    document.getElementById("title").textContent = "💡Look what I found💡"
+    
+  })
+})
